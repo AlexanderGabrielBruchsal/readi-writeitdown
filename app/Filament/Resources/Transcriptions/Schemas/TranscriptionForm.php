0089315149d30a74cd32c898036d31d3b8cbeeca
Alexander Gabriel <alexander.gabriel@bruchsal.de>
2026-02-09 14:46:03 +0100

    upgrade auf filemantphp 5, formulare und pages neue struktur

---
+++ app/Filament/Resources/Transcriptions/Schemas/TranscriptionForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\Transcriptions\Schemas;
+
+use Filament\Facades\Filament;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class TranscriptionForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('description')->label("Beschreibung")->required(),
+                Select::make("transcription_state_id")->relationship("transcription_state", "title")->disabledOn(["edit"])->visibleOn("edit"),
+                Select::make("user_id")->relationship("user", "name")->disabledOn(["edit"])->visibleOn("edit")->default(Filament::auth()->id()),
+                DateTimePicker::make("upload_time")->disabledOn(["edit"])->visibleOn("edit"),
+                DateTimePicker::make("start_time")->disabledOn(["edit"])->visibleOn("edit"),
+                DateTimePicker::make("end_time")->disabledOn(["edit"])->visibleOn("edit"),
+                FileUpload::make('attachment')->visibleOn(["create"])
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Transcriptions/Tables/TranscriptionsTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources\Transcriptions\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class TranscriptionsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('description')
+                    ->searchable(),
+                TextColumn::make('attachment')
+                    ->searchable(),
+                TextColumn::make('user.name')
+                    ->searchable(),
+                TextColumn::make('transcription_state.name')
+                    ->searchable(),
+                TextColumn::make('upload_time')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('start_time')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('end_time')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
